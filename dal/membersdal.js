6e816890af1e1f49bddf840f11befe9ceb0941fa
unknown <lizzie.olishuk1@gmail.com>
2021-07-28 18:10:49 +0300

    change mongoose uri

--- dal/membersdal.js
+++ dal/membersdal.js
@@ -6,7 +6,8 @@ GetMembers = ()=>
         return axios.get('https://jsonplaceholder.typicode.com/users')
             .then(response => {
               return response.data
-            })
+            }).catch((err)=>{
+              console.log("error:",err)})
 }
 module.exports= {GetMembers}
 
