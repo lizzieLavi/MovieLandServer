bbde91cc044236b052aace4735b5c47c9a074fb1
unknown <lizzie.olishuk1@gmail.com>
2021-07-28 17:26:48 +0300

    change mongoose uri

--- configs/database.js
+++ configs/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://lizzieLavi:olishuk2089@subscriptions.1h47i.mongodb.net/SubscriptionsDB?retryWrites=true&w=majority";
+const uri = "mongodb+srv://lizzieLavi:olishuk2089@subscriptions.1h47i.mongodb.net/SubscriptionsDB?retryWrites=true&w=majority&ssl=true&authSource=admin";
 
 
 mongoose.connect(uri).then(()=>{
