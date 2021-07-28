bfea0f7bf08881e1eb23fc8226362550ed92fba0
unknown <lizzie.olishuk1@gmail.com>
2021-07-28 16:34:34 +0300

    change mongoose uri

--- configs/database.js
+++ configs/database.js
@@ -2,5 +2,10 @@ const mongoose = require('mongoose');
 const uri = "mongodb+srv://lizzieLavi:olishuk2089@cluster0.1h47i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 
-mongoose.connect(uri)
+mongoose.connect(uri).then(()=>{
+    console.log("connected to db");
+})
+.catch((err)=>{
+    console.log("error:",err);
+})
 
