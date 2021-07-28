bcc02afdf4cf51e8d62f2c7d02480f4bd315c644
unknown <lizzie.olishuk1@gmail.com>
2021-07-28 18:05:50 +0300

    change mongoose uri

--- dal/moviesdal.js
+++ dal/moviesdal.js
@@ -6,7 +6,8 @@ GetMovies = ()=>
         return axios.get('https://api.tvmaze.com/shows')
             .then(response => {
               return response.data
-            })
+            }).catch((err)=>{
+              console.log("error:",err)})
 }
 module.exports= {GetMovies}
 
